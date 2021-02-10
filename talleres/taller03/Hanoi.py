dd43b4240eeecd21f3e2e70bba444a1806813616
Carlosvelez10798 <73538743+Carlosvelez10798@users.noreply.github.com>
2021-02-10 08:09:12 -0500

    Add files via upload

---
+++ talleres/taller03/Hanoi.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 03:10:09 2021
+
+@author: Carlos Gustavo Vélez Manco
+"""
+
+def hanoi(topN, a = "Tower1", b = "Tower2", c = "Tower3"):
+    if(topN == 1):
+        print("move "+ str(topN)+ " to "+ c)
+    else:
+        hanoi(topN-1,a,c,b)
+        print("move "+ str(topN)+ " to "+ c)
+        hanoi(topN-1,b,a,c)
+hanoi(3)
