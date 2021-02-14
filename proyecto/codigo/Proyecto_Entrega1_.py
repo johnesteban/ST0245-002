8d439f9ff377c89df6929cee7b0965d61864caaf
johnesteban <72639000+johnesteban@users.noreply.github.com>
2021-02-14 16:21:41 -0500

    Add files via upload

---
+++ proyecto/codigo/Proyecto_Entrega1_.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import os
+
+ganadoEnfermo="./ganado enfermo CSVs" 
+ganadoSano="./ganado sano CSVs"
+listaEnfermos=os.listdir(ganadoEnfermo)
+listaSanos=os.listdir(ganadoSano)#Me da los nombres de los archivos que se encuentran en la carpeta en una lista
+
+
+for i in range(len(listaEnfermos)):
+  direccion=ganadoEnfermo+"/"+listaEnfermos[i]
+  data=pd.read_csv(direccion,header=None)
+  print(data)
+
+for i in range(len(listaSanos)):
+   direccion2=ganadoSano+"/"+listaSanos[i]
+   data2=pd.read_csv(direccion2,header=None)
+   print(data2)
+
