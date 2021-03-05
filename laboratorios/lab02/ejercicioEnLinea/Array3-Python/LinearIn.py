877c3818710a19ffef0bfcfb7313e784a84cfdfc
Carlosvelez10798 <73538743+Carlosvelez10798@users.noreply.github.com>
2021-03-04 20:03:47 -0500

    Add files via upload

---
+++ laboratorios/lab02/ejercicioEnLinea/Array3-Python/LinearIn.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 12:40:00 2021
+
+@author: Carlos Gustavo Vélez Manco, John Esteban Castro Ramírez
+"""
+
+def LinearIn(outer, inner):
+    N= 0
+    SI=  False
+    for i in range(0,len(inner)):
+        for j in range(0,len(outer)):
+            if(inner[i]== outer[j]):
+                N=N+1
+                break
+    if(N==len(inner)):
+        SI= True
+    return SI
