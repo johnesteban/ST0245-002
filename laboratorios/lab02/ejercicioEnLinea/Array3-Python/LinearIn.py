8f98fa057154bbfe9a90d6a637d66b2c9ebcc758
johnesteban <72639000+johnesteban@users.noreply.github.com>
2021-03-06 22:19:35 -0500

    Add files via upload

---
+++ laboratorios/lab02/ejercicioEnLinea/Array3-Python/LinearIn.py
@@ -0,0 +1,23 @@
+def LinearIn(outer, inner):
+    N= 0 #C1
+    for i in range(0,len(inner)): #C2*n
+        for j in range(0,len(outer)): #C3*m*n
+            if(inner[i]== outer[j]): #C4*m*n
+                N=N+1 #C5*m*n
+                break #C6*m*n
+    if(N==len(inner)): #C7
+        return True #C8
+    return False #C9
+
+"""
+CÁLCULO DE COMPLEJIDAD
+T(n,m)=C1+C2*n+C3*n*m+C4*m*n+C5*m*n+C7+C8+C9
+T(n,m)=n*C2+(n*m)*(C3+C4+C5)+C--->Factor común y regla de la suma
+T(n,m)=n*C2+(n*m)*C--->Regla de la suma
+T(n,m)=n+(n*m)--->Regla del producto
+T(n,m)=n*m--->(Se toma el valor mayor, en este caso es n*m, ya que ambos valores son positivos)
+T(n,m) es O(n*m)--->Notación O
+Donde n es la longitud del arreglo inner
+Y donde m es la longitud del arreglo outer
+"""
+
