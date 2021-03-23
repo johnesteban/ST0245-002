43314e758a4f7311b50309e5801f487cabb1bdb0
johnesteban <72639000+johnesteban@users.noreply.github.com>
2021-03-23 14:34:39 -0500

    Add files via upload

---
+++ talleres/taller08/InversionPolaca.py
@@ -0,0 +1,44 @@
+class Pila:
+     def __init__(self):
+         self.items = []
+
+     def estaVacia(self):
+         return self.items == []
+
+     def incluir(self, item):
+         self.items.insert(0,item)
+
+     def extraer(self):
+         return self.items.pop(0)
+
+     def tamano(self):
+         return len(self.items)
+     
+     def operate(self,op, a, b):
+        if(op=='+'):
+            return a+b
+        elif(op=='-'):
+            return a-b
+        elif(op=='*'):
+            return a*b
+        elif(op=='/'):
+            return a/b
+        else:
+            print("Operacion no valida")
+
+class InversionPolaca():
+    def evaluate():
+        resultado = 0
+        exp = input('Ingrese la expresion a evaluar: ')
+        exp = exp.split()
+        total=Pila()
+        for i in exp:
+            if i.isnumeric():
+                total.incluir(i)
+            else:
+                resultado=total.operate(i,int(total.extraer()),int(total.extraer()))
+                total.incluir(resultado)
+        return resultado
+
+    print(evaluate())
+
