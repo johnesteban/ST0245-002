85c3548c0ba821ddbc590baa2661a04c4a39d4aa
johnesteban <72639000+johnesteban@users.noreply.github.com>
2021-03-25 19:48:26 -0500

    Add files via upload

--- laboratorios/lab03/codigo/neveras.py
+++ laboratorios/lab03/codigo/neveras.py
@@ -1,36 +1,4 @@
-
-class Pila:
-     def __init__(self):
-         self.items = []
-
-     def estaVacia(self):
-         return self.items == []
-
-     def incluir(self, item):
-         self.items.insert(0,item)
-
-     def extraer(self):
-         return self.items.pop(0)
-
-     def tamano(self):
-         return len(self.items)
-     #Porción de código tomado de: https://runestone.academy/runestone/static/pythoned/BasicDS/ImplementacionDeUnaPilaEnPython.html
-class Cola():  
-        def __init__(self):
-            self.items = []
-    
-        def estaVacia(self):
-            return self.items == []
-    
-        def agregar(self, item):
-            self.items.append(item)
-    
-        def avanzar(self):
-            return self.items.pop(0)
-    
-        def tamano(self):
-            return len(self.items)
-        
+from collections import deque        
 class Nevera():
     def __init__(self,codigo, descripcion):
         self.codigo=codigo
@@ -45,16 +13,16 @@ class Solicitudes():
         self.cantidad=cantidad
     
     def asignarSolicitudes(neveras,solicitudes):
-        if neveras.tamano()==0 or solicitudes.tamano()==0: #C1
+        if len(neveras)==0 or len(solicitudes)==0: #C1
             return "No hay nada solicitado o para asignar"  #C2
         StringFinal=""  #C3
         StringAcumulativa="" #C4
-        totalsolicitudes=solicitudes.tamano() #C5
+        totalsolicitudes=len(solicitudes) #C5
         for i in range(totalsolicitudes): #C6*n, donde n es el total de solicitudes
-            solicitud=solicitudes.avanzar() #C7*n
+            solicitud=solicitudes.popleft() #C7*n
             i=0 #C8*n
-            while neveras.tamano()>0 and i<solicitud.cantidad: #C9*n*m donde m es el numero de neveras
-                nevera=neveras.extraer() #C10*n*m
+            while len(neveras)>0 and i<solicitud.cantidad: #C9*n*m donde m es el numero de neveras
+                nevera=neveras.pop() #C10*n*m
                 StringAcumulativa+=nevera.concatenar()+',' #C11*n*m
                 i+=1 #C12*n*m
             StringFinal+=solicitud.nombreT+'-->'+'['+StringAcumulativa[:len(StringAcumulativa)-1]+']'+'\n' #C13*n
@@ -73,29 +41,29 @@ Donde n es el total de solicitudes o cantidad de tiendas que están solicitando 
 y m es el total de neveras disponibles en el almacén
 """    
 class __main__():
-    neveras=Pila()
-    neveras.incluir(Nevera(1,"haceb"))
-    neveras.incluir(Nevera(2, "lg"))
-    neveras.incluir(Nevera(3, "ibm"))
-    neveras.incluir(Nevera(4, "haceb"))
-    neveras.incluir(Nevera(5, "lg"))
-    neveras.incluir(Nevera(6, "ibm"))
-    neveras.incluir(Nevera(7, "haceb"))
-    neveras.incluir(Nevera(8, "lg"))
-    neveras.incluir(Nevera(9, "ibm"))
-    neveras.incluir(Nevera(8, "lg"))
-    neveras.incluir(Nevera(9, "ibm"))
+    neveras=deque() #Es una pila
+    neveras.appendleft(Nevera(1,"haceb"))
+    neveras.appendleft(Nevera(2, "lg"))
+    neveras.appendleft(Nevera(3, "ibm"))
+    neveras.appendleft(Nevera(4, "haceb"))
+    neveras.appendleft(Nevera(5, "lg"))
+    neveras.appendleft(Nevera(6, "ibm"))
+    neveras.appendleft(Nevera(7, "haceb"))
+    neveras.appendleft(Nevera(8, "lg"))
+    neveras.appendleft(Nevera(9, "ibm"))
+    neveras.appendleft(Nevera(8, "lg"))
+    neveras.appendleft(Nevera(9, "ibm"))
     
-    solicitudes=Cola()
-    solicitudes.agregar(Solicitudes("exito", 1))
-    solicitudes.agregar(Solicitudes("olimpica", 4))
-    solicitudes.agregar(Solicitudes("la14", 2))
-    solicitudes.agregar(Solicitudes("eafit",10))
+    solicitudes=deque() #Es una cola
+    solicitudes.append(Solicitudes("exito", 1))
+    solicitudes.append(Solicitudes("olimpica", 4))
+    solicitudes.append(Solicitudes("la14", 2))
+    solicitudes.append(Solicitudes("eafit",10))
     
     print(Solicitudes.asignarSolicitudes(neveras,solicitudes))
     
-    neveras1=Pila()
-    solicitudes1=Cola()
+    neveras1=deque()
+    solicitudes1=deque()
     print(Solicitudes.asignarSolicitudes(neveras1,solicitudes1))
     print(Solicitudes.asignarSolicitudes(neveras1,solicitudes))
     print(Solicitudes.asignarSolicitudes(neveras,solicitudes1))
